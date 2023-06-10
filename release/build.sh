0532611f0f06e1cf0eee016382d4fca745881c0b
Karl von Randow <karl@xk72.com>
2023-06-11 11:35:28 +1200

    build: improve build script

--- release/build.sh
+++ release/build.sh
@@ -1,5 +1,12 @@
 #!/bin/zsh -eu
 
+version="${1:-}"
+
+if [ -z "$version" ]; then
+  echo "usage: $0 <version>" >&2
+  exit 1
+fi
+
 ant -DRELEASE=$version -Ddebug=true
 mkdir -p ~/Development/charles/repo/com/xk72/violetlib/VAquaClient/$version
 mkdir -p  ~/.m2/repository/com/xk72/violetlib/VAquaClient/$version
