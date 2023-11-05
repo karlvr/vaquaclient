72ac584080ef8ea7f70b21e200657f6d0dddf69b
Karl von Randow <karl@xk72.com>
2023-11-06 07:29:44 +1300

    build: move sources so they don’t accumulate

--- release/build.sh
+++ release/build.sh
@@ -13,4 +13,4 @@ mkdir -p  ~/.m2/repository/com/xk72/violetlib/VAquaClient/$version
 cp dist/VAquaClient.jar ~/Development/charles/repo/com/xk72/violetlib/VAquaClient/$version/VAquaClient-$version.jar
 cp dist/VAquaClient.jar ~/.m2/repository/com/xk72/violetlib/VAquaClient/$version/VAquaClient-$version.jar
 (cd ../src && jar cf ../release/VAquaClient-$version-sources.jar .)
-cp VAquaClient-$version-sources.jar ~/.m2/repository/com/xk72/violetlib/VAquaClient/$version/
+mv VAquaClient-$version-sources.jar ~/.m2/repository/com/xk72/violetlib/VAquaClient/$version/
